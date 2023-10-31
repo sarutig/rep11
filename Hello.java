bd5166f71a4beff373f17c7254f3afe95a6e6133
sarutig <sarutigupta11@gmail.com>
2023-10-31 12:01:06 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app "); 
+    System.out.println("This is java app by using Jenkins");
+    System.out.println("This is java app 2nd build");  
+    System.out.println("This is java app new build");   
+    }  
+    }  
